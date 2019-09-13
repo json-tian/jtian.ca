d40fbacbf359ee27b50b576b17b15736ccfc7c7f
Jason Tian <tianjason6@gmail.com>
2019-09-13 17:51:21 -0400

    Removed smoothscrolling, issues on firefox

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import ReallySmoothScroll from "really-smooth-scroll";
 import App from "./App.js";
-
-ReallySmoothScroll.shim();
 
 class Main extends React.Component {
   render() {
