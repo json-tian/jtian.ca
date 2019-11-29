a8062a987f5dcdb7fbb55274833249b40912dcce
Jason Tian <tianjason6@gmail.com>
2019-11-28 21:43:52 -0500

    statcount

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Project from "../project/project.jsx"
+import Project from "../project/project.jsx";
 import "./portfolio.css";
 
 class Portfolio extends Component {
@@ -9,64 +9,88 @@ class Portfolio extends Component {
         {
           chessclock: {
             name: "Chess Clock for Mobile",
-            description: "Ordinary chess clocks are expensive, difficult, and hard to carry around. This project was inspired by the lack of features of other chess clock applicatons \
+            description:
+              "Ordinary chess clocks are expensive, difficult, and hard to carry around. This project was inspired by the lack of features of other chess clock applicatons \
             such as timeodds and custom increments.",
-            linksURL: ["https://github.com/tianjason6/Chess-Clock-Pro", "https://play.google.com/store/apps/details?id=knightsoul.chessclockv2"],
+            linksURL: [
+              "https://github.com/tianjason6/Chess-Clock-Pro",
+              "https://play.google.com/store/apps/details?id=knightsoul.chessclockv2"
+            ],
             linksICON: ["fa fa-github fa-3x", "fas fa fa-android fa-3x"],
             imageURL: "images/chessclock1.png"
           },
           spacekittens: {
             name: "Space Kittens",
-            description: "Space Kittens is a retro chain reaction game for Android devices that features over 80 unique levels. Can you beat them all?",
-            linksURL: ["https://play.google.com/store/apps/details?id=knightsoul.SpaceKittens", "https://www.youtube.com/watch?v=vsSWs31ec9c"],
+            description:
+              "Space Kittens is a retro chain reaction game for Android devices that features over 80 unique levels. Can you beat them all?",
+            linksURL: [
+              "https://play.google.com/store/apps/details?id=knightsoul.SpaceKittens",
+              "https://www.youtube.com/watch?v=vsSWs31ec9c"
+            ],
             linksICON: ["fas fa fa-android fa-3x", "fas fa fa-youtube fa-3x"],
             imageURL: "images/spacekittens1.png"
           },
           frogger: {
             name: "AI Learns to Play Frogger",
-            description: "Watch an AI learn how to play Frogger. This was created in Pygame with a simple genetic algorithm.",
-            linksURL: ["https://www.youtube.com/watch?v=LAqr7cBbOks&t=3s", "https://github.com/tianjason6/Frogger-AI"],
+            description:
+              "Watch an AI learn how to play Frogger. This was created in Pygame with a simple genetic algorithm.",
+            linksURL: [
+              "https://www.youtube.com/watch?v=LAqr7cBbOks&t=3s",
+              "https://github.com/tianjason6/Frogger-AI"
+            ],
             linksICON: ["fas fa fa-youtube fa-3x", "fa fa-github fa-3x"],
             imageURL: "images/frogger1.png"
           },
           newsarticlescraper: {
             name: "News Article Aggregator",
-            description: "News aggregation by news source and category using RSS feeds & JSoup. It also ranks the articles by interest by skimming over article keywords.",
-            linksURL: ["https://github.com/tianjason6/News-Feed-RSS-Scraper", ""],
+            description:
+              "News aggregation by news source and category using RSS feeds & JSoup. It also ranks the articles by interest by skimming over article keywords.",
+            linksURL: [
+              "https://github.com/tianjason6/News-Feed-RSS-Scraper",
+              ""
+            ],
             linksICON: ["fa fa-github fa-3x", ""],
             imageURL: "images/articleselector3.png"
           },
           minesweep: {
             name: "Auto-Generated Minesweeper",
-            description: "Generates a Minesweeper game on-command with a starter board with varying difficulty. A simple HTML-Javascript game I built when I first started learning.",
+            description:
+              "Generates a Minesweeper game on-command with a starter board with varying difficulty. A simple HTML-Javascript game I built when I first started learning.",
             linksURL: ["https://github.com/tianjason6/MineSweep", ""],
             linksICON: ["fa fa-github fa-3x", ""],
             imageURL: "images/minesweep.png"
           },
           saveme: {
             name: "Tower Conqueror Demo",
-            description: "A Tower Defence Unity3D game made for Google Cardboard VR.",
+            description:
+              "A Tower Defence Unity3D game made for Google Cardboard VR.",
             linksURL: ["https://github.com/tianjason6/Tower-Conqueror", ""],
             linksICON: ["fa fa-github fa-3x", ""],
             imageURL: "images/towerconqueror.png"
           },
           chesstrends: {
             name: "Chess Trends",
-            description: "Web scraping script to automatically fetch data on Lichess.org and Chess.com every hour and store them into a spreadsheet.",
-            linksURL: ["https://github.com/tianjason6/Google-Sheets-Scripts", "https://docs.google.com/spreadsheets/d/1enQ5aatLL3LsUVYKeuNZbD7i5GMBGS38GGZ4nVA6Pjw/edit?usp=sharing"],
+            description:
+              "Web scraping script to automatically fetch data on Lichess.org and Chess.com every hour and store them into a spreadsheet.",
+            linksURL: [
+              "https://github.com/tianjason6/Google-Sheets-Scripts",
+              "https://docs.google.com/spreadsheets/d/1enQ5aatLL3LsUVYKeuNZbD7i5GMBGS38GGZ4nVA6Pjw/edit?usp=sharing"
+            ],
             linksICON: ["fa fa-github fa-3x", "fa fa-file-o fa-3x"],
             imageURL: "images/chesstrends.png"
           },
           chessify: {
             name: "Chessify",
-            description: "Machine learning to predict the outcome of an online chess game given player rating and time control. Made using Tensorflow, Pandas in Python.",
+            description:
+              "Machine learning to predict the outcome of an online chess game given player rating and time control. Made using Tensorflow, Pandas in Python.",
             linksURL: ["https://github.com/tianjason6/Chessify", ""],
             linksICON: ["fa fa-github fa-3x"],
             imageURL: "images/chessify.png"
           },
           mplrenewal: {
             name: "Markham Public Library Auto-renewal",
-            description: "Online library book renewal, with quite literally a click of a button. Made using Selenium in Java.",
+            description:
+              "Online library book renewal, with quite literally a click of a button. Made using Selenium in Java.",
             linksURL: ["https://github.com/tianjason6/MPL_Renewal", ""],
             linksICON: ["fa fa-github fa-3x"],
             imageURL: "images/mpl.png"
@@ -74,7 +98,7 @@ class Portfolio extends Component {
         }
       ]
     }
-  }
+  };
   render() {
     return (
       <div>
@@ -92,10 +116,33 @@ class Portfolio extends Component {
               <Project data={this.state.data.Projects[0].frogger} />
               <Project data={this.state.data.Projects[0].newsarticlescraper} />
               <Project data={this.state.data.Projects[0].minesweep} />
-
             </div>
           </div>
         </div>
+        <script type="text/javascript">
+          var sc_project = 11648857; var sc_invisible = 1; var sc_security =
+          "31f3d70b";
+        </script>
+        <script
+          type="text/javascript"
+          src="https://www.statcounter.com/counter/counter.js"
+          async
+        ></script>
+        <noscript>
+          <div class="statcounter">
+            <a
+              title="Web Analytics"
+              href="https://statcounter.com/"
+              target="_blank"
+            >
+              <img
+                class="statcounter"
+                src="https://c.statcounter.com/11648857/0/31f3d70b/1/"
+                alt="Web Analytics"
+              />
+            </a>
+          </div>
+        </noscript>
       </div>
     );
   }
